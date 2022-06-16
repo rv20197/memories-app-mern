cc320705300481a10b36e20bbc964d17ecb076f0
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-06-16 22:45:40 +0530

    Connection setup done with Database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const CONNECTION_URL =
+	'mongodb+srv://vatsal:vatsal2001@nodejslearning.yc4og.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
+mongoose
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+	)
+	.catch(err => console.error(err.message));
