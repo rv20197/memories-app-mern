bc0c759c416671e20d471e03e61dc4948e3d1715
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-06-18 03:19:56 +0530

    Added Environment Variables

--- server/index.js
+++ server/index.js
@@ -2,15 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts-routes.js';
 
-const CONNECTION_URL =
-	'mongodb+srv://vatsal:vatsal2001@nodejslearning.yc4og.mongodb.net/memories?retryWrites=true&w=majority';
-
-const PORT = process.env.PORT || 5000;
-
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
@@ -19,11 +16,13 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 mongoose
-	.connect(CONNECTION_URL, {
+	.connect(process.env.CONNECTION_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true
 	})
 	.then(() =>
-		app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+		app.listen(process.env.PORT, () =>
+			console.log(`Server is running on port: ${process.env.PORT}`)
+		)
 	)
 	.catch(err => console.error(err.message));
