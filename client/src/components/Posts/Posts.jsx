d42e0f1165affe9a2fa7727d38cc30757f3095bc
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-06-18 00:07:04 +0530

    Post Card Setup done and Added Background

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
@@ -12,9 +14,21 @@ const Posts = () => {
 	console.log(posts);
 	return (
 		<>
-			<h1>Posts</h1>
-			<Post />
-			<Post />
+			{!posts.length ? (
+				<CircularProgress />
+			) : (
+				<Grid
+					className={classes.mainContainer}
+					container
+					alignItems='stretch'
+					spacing={3}>
+					{posts.map(post => (
+						<Grid key={post._id} item xs={12} sm={6}>
+							<Post post={post} />
+						</Grid>
+					))}
+				</Grid>
+			)}
 		</>
 	);
 };
