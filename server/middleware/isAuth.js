456e8b65eb0f0b55adceabaa3f89e75b1a4fe8d4
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-06-18 23:10:32 +0530

    Added auth Middleware

---
+++ server/middleware/isAuth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+const isAuth = async (req, res, next) => {
+	try {
+		const token = req.headers.authorization.split(' ')[1];
+		const isCustomAuth = token.length < 500;
+
+		let decodedData;
+
+		if (token && isCustomAuth) {
+			decodedData = jwt.verify(token, 'mern');
+
+			req.userId = decodedData?._id;
+		} else {
+			decodedData = jwt.verify(token);
+			req.userId = decodedData?.sub;
+		}
+
+		next();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export default isAuth;
--- server/routes/posts-routes.js
+++ server/routes/posts-routes.js
@@ -7,21 +7,23 @@ import {
 	likePost
 } from '../controllers/posts-controller.js';
 
+import isAuth from '../middleware/isAuth.js'
+
 const router = express.Router();
 
 // GET: http://localhost:5000/posts
 router.get('/', getPosts);
 
 // POST: http://localhost:5000/posts
-router.post('/', createPost);
+router.post('/',isAuth, createPost);
 
 // PATCH: http://localhost:5000/posts/:id
-router.patch('/:id', updatePost);
+router.patch('/:id',isAuth, updatePost);
 
 // DELETE: http://localhost:5000/posts/:id
-router.delete('/:id', deletePost);
+router.delete('/:id',isAuth, deletePost);
 
 // PATCH: http://localhost:5000/posts/:id/likePost
-router.patch('/:id/likePost', likePost);
+router.patch('/:id/likePost',isAuth, likePost);
 
 export default router;
