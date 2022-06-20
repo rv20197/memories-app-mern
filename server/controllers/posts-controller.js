40f283ee71df1321dbb9be7cb2f98098aa81939c
Vatsal Dinesh Rajgor <rv20197@gmail.com>
2022-06-20 23:31:00 +0530

    Added Pagination logic in backend

--- server/controllers/posts-controller.js
+++ server/controllers/posts-controller.js
@@ -2,9 +2,25 @@ import mongoose from 'mongoose';
 import PostMessage from '../models/postsMessage-model.js';
 
 export const getPosts = async (req, res, next) => {
+	const { page } = req.query;
 	try {
-		const postMessages = await PostMessage.find();
-		res.status(200).json(postMessages);
+		const LIMIT = 10;
+		const startIndex = (Number(page) - 1) * LIMIT; // get starting index of every page;
+
+		const total = await PostMessage.countDocuments({});
+
+		const posts = await PostMessage.find()
+			.sort({ _id: -1 })
+			.limit(LIMIT)
+			.skip(startIndex);
+
+		res
+			.status(200)
+			.json({
+				data: posts,
+				currentPage: +page,
+				numberOfPages: Math.ceil(total / LIMIT)
+			});
 	} catch (error) {
 		res.status(404).json({ message: error.message });
 	}
